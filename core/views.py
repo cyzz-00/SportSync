f8429cc24f3b3c6e7a5c4ba0cd1cb4dd5f4bc72d
Ruben Burgos <rd6burgos@gmail.com>
2026-09-16 12:53:03 -0500

    Crear páginas de inicio y acerca con navegación
    Practica con las Paginas de inicio

--- core/views.py
+++ core/views.py
@@ -3,7 +3,17 @@ from django.http import HttpResponse
 
 def inicio(request):
     return HttpResponse(
-        "<h1>SportSync</h1>"
+        "<h1><center>SportSync</h1>"
         "<p>Sistema de Gestión de Actividades y Torneos Deportivos Amateur.</p>"
         "<p>Mi primer proyecto Django está funcionando correctamente.</p>"
+        '<a href="/acerca/">Acerca del proyecto</a>'
+    )
+
+def acerca(request):
+    return HttpResponse(
+        "<h1><center>Acerca de SportSync</h1>"
+        "<p>Proyecto desarrollado para la asignatura Desarrollo de Software V.</p>"
+        "<p>Su objetivo es facilitar la organización de actividades deportivas, "
+        "la inscripción de participantes y el control de asistencia.</p>"
+        '<a href="/">Volver al inicio</a>'
     )
