e19ea2ebdd770ca23c0933907590ed3681de7db7
Ruben Burgos <rd6burgos@gmail.com>
2026-09-20 13:28:34 -0500

    Agregar Css a la pestaña de Acerca De del proyecto
    Cambiar la estetica a la pestaña acerca de

--- core/views.py
+++ core/views.py
@@ -7,10 +7,4 @@ def inicio(request):
     return render(request, "core/inicio.html")
 
 def acerca(request):
-    return HttpResponse(
-        "<h1><center>Acerca de SportSync</h1>"
-        "<p>Proyecto desarrollado para la asignatura Desarrollo de Software V.</p>"
-        "<p>Su objetivo es facilitar la organización de actividades deportivas, "
-        "la inscripción de participantes y el control de asistencia.</p>"
-        '<a href="/">Volver al inicio</a>'
-    )
+    return render(request, "core/acercade.html")
